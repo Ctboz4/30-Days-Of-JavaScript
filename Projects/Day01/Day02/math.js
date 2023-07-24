5e1d53a471bc4fd2a60cd51afbe10753a4c4a92d
Ctboz4 <Clayton.T.Boswell@outlook.com>
2023-07-25 06:06:19 +0930

    Day 2

---
+++ Projects/Day01/Day02/math.js
@@ -0,0 +1,8 @@
+let randNums = Math.random() 
+let numZeroAndTen = randNums * 11 // Will generate a randome number between 0 and 10.9999999
+console.log(numZeroAndTen)
+let randomnumbertofloor = Math.floor(numZeroAndTen) // Will remove the decimal points
+console.log(randomnumbertofloor)
+
+
+
---
+++ Projects/Day01/Day02/strings.js
@@ -0,0 +1,13 @@
+first_name = 'Clay'
+last_name = 'Moore'
+
+F_and_L_name = first_name + '' + last_name
+
+console.log(F_and_L_name)
+
+// Template strings
+
+console.log('2 plus 3 is 5')
+let a = 2
+let b = 3
+console.log(a, 'plus', b, 'is', a + b)
