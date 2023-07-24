b100fd94fd361950b7edc8398837b650fcd735b5
Ctboz4 <Clayton.T.Boswell@outlook.com>
2023-07-25 05:21:51 +0930

    Day 1 is complete

---
+++ Projects/Day01/datatypes.js
@@ -0,0 +1,9 @@
+var name = 'clay'
+var quit = false
+var none = null
+var unknown 
+
+console.log(typeof name);
+console.log(typeof quit);
+console.log(typeof unkown);
+console.log(typeof none);
---
+++ Projects/Day01/variable.js
@@ -0,0 +1,29 @@
+//Comments can make code readable
+//Welcome to 30 days of javascript
+/*comments can make code reasable, 
+easy to use, 
+and informative*/
+
+var name = 'clay'
+var quit = false
+var none = null
+var unknown 
+
+let kik 
+let go
+let age
+let class_average
+
+let horse_age = 23
+let horse_name = 'Pie-O-My'
+let horse_gender = 'Female'
+let horse_color = 'Brown'
+
+
+first_name = 'Clay'
+Last_name = "Moore"
+marital_status = 'Single'
+age = 23
+country = 'USA'
+
+first_name =  'Clay', Last_name = "Moore", marital_status = 'Single', age = 23, country = 'USA'
